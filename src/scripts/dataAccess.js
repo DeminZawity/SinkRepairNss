d14cb7c9fd802a4120821d2080132b0a933b592e
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-09-24 13:52:18 -0500

    StepSix: added a state changed custom Event. This means I don't have to refresh my page for the data to appear

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,30 +1,32 @@
-const API = "http://localhost:8088"
+const API = "http://localhost:8088";
 
 const applicationState = {
   requests: [],
-}
+};
 
 export const fetchRequests = async () => {
-  const dataFetch = await fetch(`${API}/requests`)
-  const serviceRequests= await dataFetch.json()
+  const dataFetch = await fetch(`${API}/requests`);
+  const serviceRequests = await dataFetch.json();
   // Store the external state in application state
-  applicationState.requests = serviceRequests
-}
+  applicationState.requests = serviceRequests;
+};
 
 export const getRequests = () => {
-  return applicationState.requests.map((a) => ({ ...a }))
+  return applicationState.requests.map((a) => ({ ...a }));
 };
 
 export const sendRequest = async (userServiceRequest) => {
   const fetchOptions = {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userServiceRequest)
-  }
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userServiceRequest),
+  };
 
-  const response = await fetch(`${API}/requests`, fetchOptions)
-  const responseJson = await response.json()
-  return responseJson
-}
+  const mainContainer = document.querySelector("#container");
+  const response = await fetch(`${API}/requests`, fetchOptions);
+  const responseJson = await response.json();
+  mainContainer.dispatchEvent(new CustomEvent("stateChanged"));
+  return responseJson;
+};
