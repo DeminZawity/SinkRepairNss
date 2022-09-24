ec32aecbe5539e24c5f447281bff3111172114c0
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-09-24 13:38:18 -0500

    Part Five: This was completed with an event listener.

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -15,3 +15,16 @@ export const getRequests = () => {
   return applicationState.requests.map((a) => ({ ...a }))
 };
 
+export const sendRequest = async (userServiceRequest) => {
+  const fetchOptions = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userServiceRequest)
+  }
+
+  const response = await fetch(`${API}/requests`, fetchOptions)
+  const responseJson = await response.json()
+  return responseJson
+}
